20a1ac0f102c4ae6a6f9d6ccf557523e2d028f7a
congquyen1999dev <congquyen1999dev@gmail.com>
2026-03-11 00:11:56 +0700

    #1 - define error types.

---
+++ src/BuildingBlocks/Core/Results/ErrorType.cs
@@ -0,0 +1,13 @@
+﻿namespace BuildingBlocks.Core.Results;
+
+// Type of error that can occur during an operation.
+public enum ErrorType
+{
+   Validation,
+   NotFound,
+   Conflict,
+   Unauthorized,
+   Forbidden,
+   Unexpected,
+   Internal
+}
