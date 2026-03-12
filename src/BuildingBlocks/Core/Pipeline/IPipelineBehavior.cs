303d1b84df402951f9f7ee24239d69f284ec62d8
congquyen1999dev <congquyen1999dev@gmail.com>
2026-03-13 03:06:11 +0700

    #2 - define an interface for future behaviors.

---
+++ src/BuildingBlocks/Core/Pipeline/IPipelineBehavior.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.Core.Results;
+
+namespace BuildingBlocks.Core.Pipeline;
+
+public interface IPipelineBehavior<in TRequest, TResponse>
+{
+   Task<Result<TResponse>> HandleAsync(TRequest request, RequestPipeline<TResponse> next, CancellationToken ct);
+}
