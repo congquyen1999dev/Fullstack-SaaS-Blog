06d0c42bea7af690a2b8bca38a0427ebabb2b1ce
congquyen1999dev <congquyen1999dev@gmail.com>
2026-03-13 03:04:39 +0700

    #2 - define a delegate to store in cache.

---
+++ src/BuildingBlocks/Core/Pipeline/CachedPipeline.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlocks.Core.Results;
+
+namespace BuildingBlocks.Core.Pipeline;
+
+//Store in cache
+public delegate Task<Result<TResponse>> CachedPipeline<in TRequest, TResponse>(IServiceProvider provider,
+   TRequest request, CancellationToken ct);
