f842e3d0a944ea337238fafd50692009812304ff
congquyen1999dev <congquyen1999dev@gmail.com>
2026-03-13 03:03:53 +0700

    #2 - represent a delegate that execute next step in pipeline.

---
+++ src/BuildingBlocks/Core/Pipeline/RequestPipeline.cs
@@ -0,0 +1,6 @@
+﻿using BuildingBlocks.Core.Results;
+
+namespace BuildingBlocks.Core.Pipeline;
+
+//Represents execute the next step in the pipeline and return the result.
+public delegate Task<Result<TResponse>> RequestPipeline<TResponse>();
